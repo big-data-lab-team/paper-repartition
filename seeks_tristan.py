250277f305fe33946a4c0646a8813db5447a9cbf
Tristan Glatard <tristan.glatard@concordia.ca>
2020-09-23 11:08:01 -0400

    Finished seek model

--- seeks_tristan.py
+++ seeks_tristan.py
@@ -1,44 +1,54 @@
-def get_output_seeks(A, I, R, W, O):
+import math
+
+def shape_to_end_coords(M, A, d=3):
     '''
-    A: shape of complete array (A0, A1, A2)
-    R: shape of read buffers (R0, R1, R2)
-    W: shape of write buffers (W0, W1, W2)
-    O: shape of output files (O0, O1, O2)
+    M: block shape M=(M1, M2, M3). Example: (500, 500, 500)
+    A: input array shape A=(A1, A2, A3). Example: (3500, 3500, 3500)
+    Return: end coordinates of the blocks, in each dimension. Example: ([500, 1000, 1500, 2000, 2500, 3000, 3500],
+                                                                   [500, 1000, 1500, 2000, 2500, 3000, 3500],
+                                                                   [500, 1000, 1500, 2000, 2500, 3000, 3500])
+    '''
+    return [ [ (j+1)*M[i] for j in range(int(A[i]/M[i])) ] for i in range(d)]
+
+def seeks(A, M, D):
+    '''
+    A: shape of the large array. Example: (3500, 3500, 3500)
+    M: coordinates of memory block ends (read or write). Example: ([500, 1000, 1500, 2000, 2500, 3000, 3500],
+                                                                   [500, 1000, 1500, 2000, 2500, 3000, 3500],
+                                                                   [500, 1000, 1500, 2000, 2500, 3000, 3500])
+    D: coordinates of disk block ends (input or output). Example: ([500, 1000, 1500, 2000, 2500, 3000, 3500],
+                                                                   [500, 1000, 1500, 2000, 2500, 3000, 3500],
+                                                                   [500, 1000, 1500, 2000, 2500, 3000, 3500])
+    Returns: number of seeks required to write M blocks into D blocks. This number is also the number of seeks
+             to read D blocks into M blocks.
     '''
 
-    n = 3  # dimension of the array
-    c = [ 0 for i in range(n)] # number of cuts in each dimension
+    c = [ 0 for i in range(len(A))] # number of cuts in each dimension
     m = [] # number of matches in each dimension
 
-    no = 1  # Total number of output blocks
-    for d in range(n):
-        no *= A[d]/O[d]
-    ni = 1  # Total number of input blocks
-    for d in range(n):
-        ni *= A[d]/I[d] 
+    n = math.prod( [len(D[i]) for i in range(len(A))])  # Total number of disk blocks
 
-    for d in range(n): # d is the dimension index
-        assert(A[d] % W[d] == 0) # Ad needs to be a multiple of Wd
-        assert(A[d] % O[d] == 0) # Ad needs to be a multiple of Od
-        nw = int(A[d]/W[d]) # number of write blocks in dimension d
-        no = int(A[d]/O[d]) # number of output blocks in dimension d
-
-        Wd = [ i*W[d] for i in range(1, nw+1)] # coordinates of the write block endings
-        Od = [ i*O[d] for i in range(1, no+1)] # coordinates of the output block endings
+    for d in range(len(A)): # d is the dimension index
+        
+        nd = len(D[d])
         Cd = [ ]  # all the cut coordinates (for debugging and visualization)
-        for i in range(no): # for each output block, check how many pieces need to be written
-            oi = i*O[d] # beginning of the block
-            Cid = [ w for w in Wd if oi < w and w < oi+O[d] ]  # number of write block endings in the output block
+        for i in range(nd): # for each output block, check how many pieces need to be written
+            if i == 0:
+                Cid = [ m for m in M[d] if 0 < m and m < D[d][i] ]  # number of write block endings in the output block
+            else:               
+                Cid = [ m for m in M[d] if D[d][i-1] < m and m < D[d][i] ]  # number of write block endings in the output block
             if len(Cid) == 0:
                 continue
             c[d] += len(Cid) + 1
             Cd += Cid
 
-        m.append(len(set(Wd).union(set(Od))) - c[d])
+        m.append(len(set(M[d]).union(set(D[d]))) - c[d])
 
-    s = A[0]*A[1]*c[2] + A[0]*c[1]*m[2] + c[0]*m[1]*m[2] + no + ni
+    s = A[0]*A[1]*c[2] + A[0]*c[1]*m[2] + c[0]*m[1]*m[2] + n
 
     return s
+
+## Test cases
 
 cases = { # cases used for the paper (table 1)
         "case 1_0": {
@@ -72,8 +82,12 @@ cases = { # cases used for the paper (table 1)
                 "ref": 4
         },
     }
+
+
 for c in cases:
-    s = get_output_seeks(cases[c]["A"],
-                         cases[c]["I"], cases[c]["I"],
-                         cases[c]["I"], cases[c]["O"])
-    print(s) 
+    A = cases[c]["A"]
+    M = cases[c]["I"]  # for baseline, I=R=W
+    D = cases[c]["O"]
+    ni = int(math.prod( A[d]/M[d] for d in range(3)))
+    s = seeks(A, shape_to_end_coords(M, A), shape_to_end_coords(D, A)) + ni
+    print(s)
